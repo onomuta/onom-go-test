122cd9b55e94d74cb6f95eba413e55a078048dd8
Takao Onomura <takao.onomura@kameda-hi.co.jp>
2018-07-05 11:45:19 +0900

    hoge

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+  "github.com/gin-gonic/gin"
+  "net/http"
 )
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":"+port, nil)
+type C struct {
+  Id int
+  Name string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hellooo00000, %q", r.URL.Path[1:])
+func main() {
+  router := gin.Default()
+  router.LoadHTMLGlob("templates/*.tmpl")
+  router.GET("/index", func(c *gin.Context) {
+    c.HTML(http.StatusOK, "index.tmpl", gin.H{
+      "a": "a",
+      "b": []string{"b_todo1","b_todo2"},
+      "c": []C{{1,"c_mika"},{2,"c_risa"}},
+      "d": C{3,"d_mayu"},
+      "e": true,
+      "f": false,
+      "h": true,
+    })
+  })
+  router.Run(":8080")
 }
